75feb1794c214bbb1c426f108d2e25fbf49283e9
kshen5 <kshen5@my.centennialcollege.ca>
2018-09-08 23:49:09 -0400

    Initial COmmit

---
+++ Scripts/game.js
@@ -0,0 +1,12 @@
+//IIFE - Immediately Invoked Funtion Expression
+(function(){
+
+    function Start() {
+        console.log(`%c Game Started`, "color: blue; font-size: 20px;");
+    }
+
+    function Update() {
+
+    }
+    window.addEventListener("load", Start);
+})();
