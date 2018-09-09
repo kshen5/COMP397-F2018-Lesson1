9ac06ab51131afca0b165a4dbafb6ec61ed6866f
kshen5 <kshen5@my.centennialcollege.ca>
2018-09-09 01:01:42 -0400

    added simple label and button

--- Scripts/core/game.js
+++ Scripts/core/game.js
@@ -1,10 +1,45 @@
 //IIFE - Immediately Invoked Funtion Expression
 (function () {
-    var myVariable = 5;
+    // game vaiables
+    var canvas;
+    var stage;
+    var helloLabel;
+    var clickMeButton;
     function Start() {
         console.log("%c Game Started...", "color: blue; font-size: 20px;");
+        canvas = document.getElementsByTagName("canvas")[0];
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60; // game will run at 60fps
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
+    // this is the main game loop
     function Update() {
+        stage.update();
+    }
+    function Main() {
+        helloLabel = new createjs.Text("Hello, World!", "60px Consolas", "#000000");
+        helloLabel.regX = helloLabel.getBounds().width * 0.5;
+        helloLabel.regY = helloLabel.getBounds().height * 0.5;
+        helloLabel.x = 320;
+        helloLabel.y = 240;
+        stage.addChild(helloLabel);
+        clickMeButton = new createjs.Bitmap("/Assets/images/clickMeButton.png");
+        clickMeButton.regX = helloLabel.getBounds().width * 0.5;
+        clickMeButton.regY = helloLabel.getBounds().height * 0.5;
+        clickMeButton.x = 320;
+        clickMeButton.y = 360;
+        stage.addChild(clickMeButton);
+        clickMeButton.on("click", function () {
+            helloLabel.text = "Clicked!";
+        });
+        clickMeButton.on("mouseover", function () {
+            clickMeButton.alpha = 0.7;
+        });
+        clickMeButton.on("mouseoutr", function () {
+            clickMeButton.alpha = 1.0;
+        });
     }
     window.addEventListener("load", Start);
 })();
--- Scripts/core/game.ts
+++ Scripts/core/game.ts
@@ -1,13 +1,53 @@
 //IIFE - Immediately Invoked Funtion Expression
 (function(){
-    let myVariable:number = 5;
+    // game vaiables
+    let canvas:HTMLCanvasElement;
+    let stage:createjs.Stage;
+    let helloLabel:createjs.Text;
+    let clickMeButton:createjs.Bitmap;
 
-    function Start() {
+    function Start():void {
         console.log(`%c Game Started...`, "color: blue; font-size: 20px;");
+        canvas = document.getElementsByTagName("canvas")[0];
+        stage = new createjs.Stage(canvas);
+        stage.enableMouseOver(20);
+        createjs.Ticker.framerate = 60; // game will run at 60fps
+        createjs.Ticker.on("tick", Update);
+        Main();
     }
 
-    function Update() {
-
+    // this is the main game loop
+    function Update():void {
+        stage.update();
     }
+
+    function Main():void {
+        helloLabel = new createjs.Text("Hello, World!", "60px Consolas", "#000000");
+        helloLabel.regX = helloLabel.getBounds().width * 0.5;
+        helloLabel.regY = helloLabel.getBounds().height * 0.5;
+        helloLabel.x = 320;
+        helloLabel.y = 240;
+        stage.addChild(helloLabel);
+
+        clickMeButton = new createjs.Bitmap("/Assets/images/clickMeButton.png");
+        clickMeButton.regX = helloLabel.getBounds().width * 0.5;
+        clickMeButton.regY = helloLabel.getBounds().height * 0.5;
+        clickMeButton.x = 320;
+        clickMeButton.y = 360;
+        stage.addChild(clickMeButton);
+
+        clickMeButton.on("click", function() {
+            helloLabel.text = "Clicked!";
+        });
+
+        clickMeButton.on("mouseover", function() {
+            clickMeButton.alpha = 0.7;
+        });
+
+        clickMeButton.on("mouseoutr", function() {
+            clickMeButton.alpha = 1.0;
+        });
+    }
+
     window.addEventListener("load", Start);
 })();
